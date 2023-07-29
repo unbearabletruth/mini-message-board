3c6409abe994f91788c1b3d675a454d3b8bd32f5
unbearabletruth <antaarty@gmail.com>
2023-07-29 13:49:47 +0700

    simple css

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
+const convertedDate = () => {
+  let date = new Date;
+  let stringDate = date.toLocaleString('en-us', {
+    month: "short",
+    day: "2-digit",
+    hour: "numeric", minute: "numeric"
+  });
+  return stringDate
+}
+
 const messages = [
   {
     text: "Hi there!",
     user: "Amando",
-    added: new Date()
+    added: convertedDate()
   },
   {
     text: "Hello World!",
     user: "Charles",
-    added: new Date()
+    added: convertedDate()
   }
 ];
 
@@ -21,7 +31,8 @@ router.get('/', (req, res, next) => {
 router.post('/new', (req, res, next) => {
   const messageText = req.body.messageText;
   const authorName = req.body.authorName;
-  messages.push({text: messageText, user: authorName, added: new Date()});
+  messages.push({text: messageText, user: authorName, 
+    added: convertedDate()});
   res.redirect('/')
 });
 
