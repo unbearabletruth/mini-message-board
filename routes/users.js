417b1ac122d3b3b3d4d17611de821ae415cdd720
unbearabletruth <antaarty@gmail.com>
2023-07-29 11:05:40 +0700

    setup

---
+++ routes/users.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+router.get('/', (req, res, next) => {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
