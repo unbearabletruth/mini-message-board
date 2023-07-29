683a975dcf9efc3158053e2128c8d6db6d9ccff2
unbearabletruth <antaarty@gmail.com>
2023-07-29 11:56:42 +0700

    index and new message views

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+router.get('/', (req, res, next) => {
+  res.render('index', { title: "Mini Messageboard", messages: messages })
 });
 
 module.exports = router;
